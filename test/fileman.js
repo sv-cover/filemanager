c9db744fbd371cd040785884a48eefd2af1a4ad0
Nico Stegeman <nicostegeman89@gmail.com>
2020-02-19 14:09:37 +0100

    Reworked test files

--- test/fileman.js
+++ test/fileman.js
@@ -1,13 +1,13 @@
-var Fileman = function(url) {
+var Fileman = function(url, funcShow, funcClose) {
   // Set the url to the fileman app
   var filemanURL = url;
   var imageResizeURL = filemanURL + '/images/resize';
 
   function _nothing() { return };
 
-  this._funcShow = _nothing;
+  this._funcShow = funcShow;
   this._funcSelect = _nothing;
-  this._funcClose = _nothing;
+  this._funcClose = funcClose;
   
   // Initialise the api by adding a listener for the message event
   window.addEventListener('message', event => {
@@ -35,47 +35,20 @@ var Fileman = function(url) {
     The function funcShow should close the element containing the fileman iframe.
     The function funcSelect(file) gets the selected fileMetadata from the fileman app.
   */
- this.connect = function(funcShow, funcClose, funcSelect) {
-    return () => {
-      this._funcShow = funcShow;
-      this._funcClose = funcClose;
+ this.connect = function(funcSelect) {
+    console.log(this);
+    return (event) => {
+      event.preventDefault();
       this._funcSelect = funcSelect;
       this._funcShow();
     }
   };
 
-  this.connectInputOnClick = function(funcShow, funcClose, input, options) {
+  this.connectInputOnClick = function(input, options) {
     document.querySelectorAll(input).forEach(elem => {
-      elem.addEventListener('click', this.connect(funcShow, funcClose, file => {
+      elem.addEventListener('click', this.connect(file => {
         elem.value = this.createUrl(file, options);
       }));
-    });
-  };
-
-  this.connectImgOnClick = function(funcShow, funcClose, img, options) {
-    document.querySelectorAll(img).forEach(elem => {
-      elem.onclick = this.connect(funcShow, funcClose, file => {
-        elem.src = this.createUrl(file, options);
-      });
-    });
-  };
-
-  this.connectPictures = function(funcShow, funcClose, img, widths) {
-    document.querySelectorAll(img).forEach(elem => {
-      elem.onclick = this.connect(funcShow, funcClose, file => {
-        var html = '';
-        var w = null;
-        var i = 0;
-
-        for(i = 0; i < (widths.length -1); i = i + 1) {
-          w = widths[i];
-          html = html + '<source media="(min-width: ' + w.toString() + 'px)" srcset="' + _createUrl(file, {w: w}) + '">';
-        }
-        w = widths[widths.length - 1];
-        html = html + '<img src="' + _createUrl(file, {w: w}) + '" alt="' + file.name + '" style="width:auto;"">';
-
-        elem.innerHTML = html;
-      });
     });
   };
 }
