c8f1b215546236dcfde72a83abbebaed0835b337
Nico Stegeman <nicostegeman89@gmail.com>
2020-03-06 13:43:32 +0100

    Changed configs to make the secret smaller

--- config/coverapi-default.js
+++ config/coverapi-default.js
@@ -1,10 +1,4 @@
 const coverapi = {
-  /* Cover API */
-  'COVER_API_URL': 'http://host.docker.internal:8080/api.php',
-  'COVER_LOGIN_URL': 'http://localhost:8080/api.php?view=login',
-  'COVER_LOGOUT_URL': 'http://localhost:8080/api.php?view=logout',
-  'COVER_COOKIE': 'cover_session_id',
-  'COVER_API_APP': 'test-app',
   'COVER_API_SECRET': 'ultrasecrethashkey'
 }
 
--- config/index-default.js
+++ config/index-default.js
@@ -13,7 +13,13 @@ const config = { ...{
     /* Caching */
     'CACHE_USE': true,
     'CACHE_MAX_SIZE': 100*1024*1024, /* In bytes. */
-    'CACHE_MAX_AGE': 24*60*60*1000 /* In miliseconds */
+    'CACHE_MAX_AGE': 24*60*60*1000, /* In miliseconds */
+    /* Cover API */
+    'COVER_API_URL': 'http://host.docker.internal:8080/api.php',
+    'COVER_LOGIN_URL': 'http://localhost:8080/api.php?view=login',
+    'COVER_LOGOUT_URL': 'http://localhost:8080/api.php?view=logout',
+    'COVER_COOKIE': 'cover_session_id',
+    'COVER_API_APP': 'test-app',
 }, ...coverapi, ...fileman }
 
 module.exports = config;
