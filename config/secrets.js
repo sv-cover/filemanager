7a631257efaea8f78c2459c715bd4a711da99819
Martijn Luinstra <martijnluinstra@gmail.com>
2023-01-24 23:02:56 +0100

    Setup dev enviroment

--- config/secrets.js
+++ config/secrets.js
@@ -1,6 +1,6 @@
 const secrets = {
   'COVER_API_SECRET': 'ultrasecrethashkey',
-  'REDIS_PASSWORD': 'a93F7K7kMBbphxNdeJeUvmlpmU2oMop19a3Kz4dnUY7BagqXaebPvdc1eb4AyP4n'
+  'REDIS_PASSWORD': ''
 }
 
 module.exports = secrets;
